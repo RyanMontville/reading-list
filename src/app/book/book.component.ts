939b304930f96bb99f7aa2362b094eb80dce8c28
RyanMontville <ryan.a.montville@gmail.com>
2025-04-17 14:59:02 -0500

    fixing format of date read

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -13,4 +13,25 @@ export class BookComponent {
   formatAuthorsForRoute(author: string) {
     return "/reading-list/author/" + author.replace(" ", "-");
   }
+
+  formatDate(date: string) {
+    let dateList = date.split("/")
+    let month = dateList[0];
+    switch(month) {
+      case "01": return `January ${dateList[2]}`;
+      case "02": return `February ${dateList[2]}`;
+      case "03": return `March ${dateList[2]}`;
+      case "04": return `April ${dateList[2]}`;
+      case "05": return `May ${dateList[2]}`;
+      case "06": return `June ${dateList[2]}`;
+      case "07": return `July ${dateList[2]}`;
+      case "08": return `August ${dateList[2]}`;
+      case "09": return `September ${dateList[2]}`;
+      case "10": return `October ${dateList[2]}`;
+      case "11": return `November ${dateList[2]}`;
+      case "12": return `December ${dateList[2]}`;
+      default: return "Error.Couldn't load date.";
+
+    }
+  }
 }
