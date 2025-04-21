a5101a19fc8f08d3631b077d9ce368d943101532
RyanMontville <ryan.a.montville@gmail.com>
2025-04-21 16:47:30 -0500

    commitiing to update node modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
+import { MenuComponent } from "./menu/menu.component";
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, MenuComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  animations: [
+    trigger('cardAnimation', [
+      state('in', style({ opacity: 1, transform: 'translateX(0)' })),
+      state('out', style({ opacity: 0, transform: 'translateX(100%)' })),
+      transition('void => in', [ // Animate in from the left
+        style({ opacity: 0, transform: 'translateX(-100%)' }),
+        animate('300ms ease-in')
+      ]),
+      transition('in => out', [
+        animate('300ms ease-out')
+      ]),
+      transition('void => *', animate('0ms')),
+    ]),
+  ],
 })
 export class AppComponent {
   title = 'reading-list';
+  showMenu: boolean = false;
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -10,6 +11,7 @@ export const appConfig: ApplicationConfig = {
       { eventCoalescing: true }
     ), 
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    provideAnimationsAsync()
   ]
 };
