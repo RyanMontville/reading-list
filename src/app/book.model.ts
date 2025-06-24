f22ac4828ab17cbf1ded5a162d364faee8f4d470
Monte <ryanamontville@gmail.com>
2025-06-24 11:22:06 -0400

    removing year from book object

--- src/app/book.model.ts
+++ src/app/book.model.ts
@@ -12,3 +12,15 @@ export interface BookLink {
   year: string;
   books: Book[];
 }
+
+export interface Vlogbrothers {
+  book_id: number;
+  book_title: string;
+  author: string;
+  who_recommended: string;
+  media_title: string;
+  recommended_on: string;
+  first_recommended: string;
+  type: string;
+  cover_url: string;
+}
--- src/app/books.service.ts
+++ src/app/books.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Book } from './book.model';
+import { Book, Vlogbrothers } from './book.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BookService {
-  private booksUrl = 'https://raw.githubusercontent.com/RyanMontville/reading-list/refs/heads/main/books.json'; // Path to your JSON
+  private booksUrl = 'https://raw.githubusercontent.com/RyanMontville/reading-list/refs/heads/main/books.json';
 
   constructor(private http: HttpClient) {}
 
   getBooks(): Observable<Book[]> {
     return this.http.get<Book[]>(this.booksUrl);
+  }
+
+  getVlogbrothers(): Observable<Vlogbrothers[]> {
+    return this.http.get<Vlogbrothers[]>("https://raw.githubusercontent.com/RyanMontville/reading-list/refs/heads/main/vlogbrothers_list.json");
   }
 }
